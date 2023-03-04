f9911190aae488f50b940c33751513c54fc36a7d
Matteo Emanuele Russo <matteoh8@hotmail.it>
2023-03-04 14:57:52 +0100

    transitions added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <TheNavigation />
-    <router-view />
+    <transition name="moveUp">
+      <router-view :key="$route.path"/>
+    </transition>
   </div>
 </template>
 
@@ -23,6 +25,41 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+/* SLIDE */
+.slide-enter-active,
+.slide-leave-active {
+  transition: opacity .3s, transform .3s;
+}
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  transform: translateX(-30%);
+}
+/* MOVEUP */
+.moveUp-enter-active {
+  animation: fadeIn .6s ease-in;
+}
+@keyframes fadeIn {
+  0%{opacity: 0;}
+  50%{opacity: .5;}
+  100%{opacity: 1;}
+}
+.move-leave-active {
+  animation: moveUp 1s ease-in;
+}
+@keyframes moveUp {
+  0%{tranform: translateY(0);}
+  100%{tranform: translateY(-400px);}
+}
+/* FADE */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter-to,
+.fade-leave-to {
+  opacity: 0;
 }
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="home">
     <h1>All Destinations</h1>
+    <div class="destinations-container">
+      <div class="all-destinations"
+      v-for="destination in destinations" 
+      :key="destination.name">
 
-    <div class="all-destinations"
-    v-for="destination in destinations" 
-    :key="destination.name">
-
-      <router-link :to="{name: 'DestinationDetails', params: {slug: destination.slug}}">
-        <h4>{{ destination.name }}</h4>
-        <figure>
-          <img :src="require(`@/assets/${destination.image}`)" 
-          :alt="destination.name">
-        </figure>
-      </router-link>
-      
+        <router-link :to="{name: 'DestinationDetails', params: {slug: destination.slug}}">
+            <h4>{{ destination.name }}</h4>
+            <figure>
+              <img :src="require(`@/assets/${destination.image}`)" 
+              :alt="destination.name" class="destination-img">
+            </figure>
+        </router-link>
+        
+      </div>
     </div>
   </div>
 </template>
@@ -35,3 +36,19 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.destinations-container {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  gap: 1rem;
+}
+.destination-img {
+  max-width: 70%;
+}
+</style>
